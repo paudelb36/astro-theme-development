9a716063e985e2ba6994c7036c6f8643c3706f8c
paudelb36 <paudelb11@gmail.com>
2025-04-09 16:37:59 +0545

    New Pages added

---
+++ src/data/cardDetails.js
@@ -0,0 +1,20 @@
+export const cardDetails = [
+    {
+      title: "The Art of Balancing Form and Function in Architecture",
+      description:
+        "The delicate dance between aesthetics and utility as we delve into the art of harmonizing form and function in contemporary architectural masterpieces.",
+      image: "https://twentytwentyfourdemo.wordpress.com/wp-content/uploads/2023/11/0565b-tt4_post_1.jpg",
+    },
+    {
+      title: "Impressive Buildings",
+      description:
+        "They inspire us to think beyond convention and remind us that buildings can become extraordinary works of art.",
+      image: "https://twentytwentyfourdemo.wordpress.com/wp-content/uploads/2023/11/1ceb1-tt4_post_2.jpg?w=2048",
+    },
+    {
+      title: "Exploring Innovative Materials Shaping the Future of Architecture",
+      description:
+        "The delicate dance between aesthetics and utility as we delve into the art of harmonizing form and function in contemporary architectural masterpieces.",
+      image: "https://twentytwentyfourdemo.wordpress.com/wp-content/uploads/2023/11/b3f2c-tt4_post_3-scaled-1.jpg?w=2048",
+    },
+  ];
